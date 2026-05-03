589c2e1f59cfd22f8395008087ffb5ec7672c009
Denzel Navarro <denzel.navarro@elinnovtech.com>
2026-05-03 13:22:53 +0800

    Feat: Add parallel email sending for attendance reminders

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -183,6 +183,8 @@ const attendanceReminderCron = inngest.createFunction(
       });
     }
 
+    await Promise.all(emailPromises)
+
     return {
       totalActive: activeEmployees.length,
       onLeave: onLeaveIds.length,
